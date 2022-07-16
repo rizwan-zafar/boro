342768c2a9dc415e09663642808b37fae582e7b9
rizwan-zafar <62982836+rizwan-zafar@users.noreply.github.com>
2022-07-15 17:24:55 -0700

    Update contact-support.tsx

--- src/components/contact/contact-support.tsx
+++ src/components/contact/contact-support.tsx
@@ -1,36 +1,7 @@
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
-import Text from '@components/ui/text';
-import Image from '@components/ui/image';
-import Heading from '@components/ui/heading';
-import Link from '@components/ui/link';
 
-const supportData = [
-  {
-    id: 1,
-    slug: '/',
-    name: 'Jessica Twain',
-    thumbnail: '/assets/images/support/1.png',
-  },
-  {
-    id: 1,
-    slug: '/',
-    name: 'Jason',
-    thumbnail: '/assets/images/support/2.png',
-  },
-  {
-    id: 1,
-    slug: '/',
-    name: 'Jessica',
-    thumbnail: '/assets/images/support/3.png',
-  },
-  {
-    id: 1,
-    slug: '/',
-    name: 'Hayden',
-    thumbnail: '/assets/images/support/4.png',
-  },
-];
+ 
 
 interface Props {
   image?: HTMLImageElement;
@@ -39,27 +10,13 @@ interface Props {
 const ContactSupport: FC<Props> = () => {
   const { t } = useTranslation('common');
   return (
-    <div className="mb-0 3xl:ltr:pr-5 3xl:rtl:pl-5">
-      <Heading variant="heading" className="mb-3 lg:mb-4 xl:mb-5">
-        {t('contact-form-info-title')}
-      </Heading>
-      <Text className="xl:leading-8">{t('contact-form-info-content')}</Text>
-      <div className="flex mt-5 -mx-1">
-        {supportData.map((item, idx) => (
-          <Link
-            href={item.slug}
-            key={idx}
-            className="mx-1 transition-all shrink-0 hover:opacity-90"
-          >
-            <Image
-              src={item.thumbnail}
-              alt={item.name}
-              width={45}
-              height={45}
-            />
-          </Link>
-        ))}
-      </div>
+    <div  >
+      
+       
+             <img src="https://media.istockphoto.com/photos/how-can-i-help-you-picture-id519122924?k=20&m=519122924&s=612x612&w=0&h=ffQVbTzttHSi15gbCxdB21o2sMGFYK2ZyyLgs6iPiK4=" />
+          
+       
+       
     </div>
   );
 };
